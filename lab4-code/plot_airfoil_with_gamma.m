d90bd13e51e51359cb3934907d8b5a964a0349c3
Jacob Killelea <Jkillelea@protonmail.ch>
2017-10-20 21:58:38 -0600

    handle kutta contition

--- lab4-code/plot_airfoil_with_gamma.m
+++ lab4-code/plot_airfoil_with_gamma.m
@@ -9,21 +9,25 @@ function plot_airfoil_with_gamma(x, y, gamma)
 
   for i = 1:N-1
     theta = atan((y(i+1)-y(i))/(x(i+1)-x(i)));
-    % theta = atan2((y(i+1)-y(i)), (x(i+1)-x(i)));
     theta = theta + pi/2; % rotate to point to the normal
-    % n = [cos(theta), sin(theta)]; % x, y
 
     gx(i) = x(i) + gamma(i)*cos(theta);
     gy(i) = y(i) + gamma(i)*sin(theta);
   end
 
-  figure;
-  hold on;
-  grid on;
-  plot(x, y);
-  plot(gx, gy);
+  % Kutta condition
+  gx(end) = gx(1);
+  gy(end) = -gy(1);
+
+  figure
+  hold on
+  grid on
+  axis equal
+  plot(x, y)
+  plot(gx, gy)
+
   for i = 1:N
     plot([x(i), gx(i)], [y(i), gy(i)])
   end
-  axis equal
+
 end
